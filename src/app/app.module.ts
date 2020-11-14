d66598aa47c8a274936c9e16891260d685a63f23
cristian_torres28 <cristiana.torresc@konradlorenz.edu.co>
2020-11-13 23:09:13 -0500

    Commit inicial estructura base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginAdminComponent } from './components/login-admin/login-admin.component';
+import { LoginTeacherComponent } from './components/login-teacher/login-teacher.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { AsignaturaComponent } from './components/asignatura/asignatura.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { CalificacionesComponent } from './components/calificaciones/calificaciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginAdminComponent,
+    LoginTeacherComponent,
+    TeachersComponent,
+    AsignaturaComponent,
+    CargaComponent,
+    CalificacionesComponent
   ],
   imports: [
     BrowserModule,
